d2cfa6c05a5bb4fce874326abbc9f3a8ad6fdc2a
Olaf Gomez <olaf.gomez.freelance@gmail.com>
2022-02-11 18:29:11 +0100

    MyChanges

---
+++ Assets/Optimization/Scripts/Raycaster.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// casts a Physics Raycast from the users mouse input to check if it hits the floor
+/// </summary>
+public class Raycaster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit)){
+            
+                if (hit.transform.gameObject.name.Contains("Floor"))
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        FindObjectOfType<PlayerController>().SetPlayerPosition(hit.point);
+                    
+                    }
+
+
+            }
+
+            }
+    }
+}
